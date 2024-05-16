e0d4d6507120d7e518ee5d9caafe9a360ebaf3bc
kais Alkotamy <kais.alkotamy@gmail.com>
2024-05-17 00:30:03 +0400

    fix filter --> nest js filter

--- src/app/_models/Tag.ts
+++ src/app/_models/Tag.ts
@@ -4,7 +4,7 @@ export class Tag{
   static readonly JAVA = new Tag('Java','blue')
   static readonly SPRINGBOOT = new Tag('Spring Boot','green')
   static readonly TYPESCRIPT = new Tag('Type Script','pink')
-  static readonly NESTJS = new Tag('Nest js','red')
+  static readonly NESTJS = new Tag('Nest Js','red')
   private constructor(public key:string, public color:string){}
   toString(){
     return this.key;
